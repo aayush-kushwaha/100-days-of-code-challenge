d70a4bd6509c90317971f0393c81a02b9de193a3
Aayush Kushwaha <1728107@kiit.ac.in>
2022-05-29 21:58:25 +0530

    Day-1-edited

---
+++ Day-1/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print('Welcome to Band Name Generator')
+city = input("In which city you grew up in?\n")
+pet = input(" Whats the name of your pet?\n")
+band_name = (print("Your band name can be: " + city + pet))
